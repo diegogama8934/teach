68991291ed53b5121664e9fbdb19adf3c1cd4cf9
Diego Martinez <diegogama8934@gmail.com>
2024-07-26 13:59:07 -0600

    add: add announcement component showed in announcements page

--- app/dashboard/subject/[subjectId]/student/announcements/page.tsx
+++ app/dashboard/subject/[subjectId]/student/announcements/page.tsx
@@ -1,5 +1,4 @@
-import { Header } from "@/components";
-import Image from "next/image";
+import { Header, AddAnnouncement } from "@/components";
 interface Props {
   params: { subjectId: string }
 }
@@ -20,17 +19,7 @@ export default async function StudentAnnouncementsPage({ params }: Props) {
       <Header title={`${subjectName} - Anuncios de clase`} primaryAction={<></>} />
 
       <main className="flex flex-col flex-1 w-full bg-white rounded-3xl p-8">
-        <div className="bg-gray-100 px-2">
-          <Image
-            src="/yo3.jpeg"
-            width={40}
-            height={40}
-            alt="Foto de perfil"
-            className="w-10 aspect-square object-cover rounded-full"
-          />
-          <textarea placeholder="Comparto algo para la clase" className="bg-gray-100 py-3 px-4 rounded-lg focus-visible:outline-none" />
-
-        </div>
+        <AddAnnouncement />
       </main>
     </>
   );
--- components/atoms/subject/AddAnnouncement.tsx
+++ components/atoms/subject/AddAnnouncement.tsx
@@ -1,9 +1,39 @@
-export function AddAnnouncement() {
+"use client";
+import { useRef, useState } from "react";
+import Image from "next/image";
 
+export function AddAnnouncement() {
+  const [form, setForm] = useState({
+    value: ""
+  });
+  const textareaInput = useRef<HTMLTextAreaElement>(null);
 
   return (
     <>
-      <h1></h1>
+      <form>
+        <div
+          className="bg-gray-50 hover:bg-gray-100 p-4 flex items-start gap-2 rounded-lg hover:scale-[101%] cursor-text transition-all"
+          onClick={() => {
+            console.log(textareaInput.current);
+            textareaInput.current?.focus()
+          }}
+        >
+          <Image
+            src="/yo3.jpeg"
+            width={40}
+            height={40}
+            alt="Foto de perfil"
+            className="w-10 aspect-square object-cover rounded-full"
+          />
+
+          <textarea
+            placeholder="Comparto algo para la clase"
+            className="flex-1 bg-transparent py-2 px-2 rounded-lg focus-visible:outline-none"
+            ref={textareaInput}
+          />
+
+        </div>
+      </form>
     </>
   );
 }
