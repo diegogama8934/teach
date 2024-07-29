b08239d884ae9a336d1221e25b86431b5ed02625
Diego Martinez <diegogama8934@gmail.com>
2024-07-28 21:04:37 -0600

    add: 404 not found page

--- app/dashboard/subject/[subjectId]/page.tsx
+++ app/dashboard/subject/[subjectId]/page.tsx
@@ -3,6 +3,7 @@ import { useContext } from "react";
 import { UserContext } from "@/context/UserContext";
 import { useParams, useRouter } from "next/navigation";
 
+// TODO: Make this a server component
 
 export default function RedirectPage() {
     const params = useParams();
---
+++ app/not-found.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function NotFoundPage() {
+
+
+  return (
+    <div className="flex flex-col w-full h-full items-center relative bg-white pt-20">
+      <div className="flex flex-col gap-10 items-center">
+        <h1 className="text-7xl font-bold tracking-widest">404</h1>
+        <h2 className="text-xl">Ooops! We don´t know this page</h2>
+      </div>
+
+      <Image
+        src="/not-found-image.svg"
+        width={450}
+        height={300}
+        alt="Not found image"
+        className="absolute bottom-0"
+      />
+    </div>
+  );
+}
